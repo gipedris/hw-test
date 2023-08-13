ebb3b36d0eef97a28a8746aca387bbd8742cb240
Кальнов Иван Юрьевич <IYKalnov@sberbank.ru>
2023-08-13 03:57:01 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,57 +10,54 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(sr string) (string, error) {
-
-	var last_char strings.Builder
-	var full_string strings.Builder
-	var last_char_is_digit bool
+	var lastChar strings.Builder
+	var fullString strings.Builder
+	var lastCharIsDigit bool
 
 	for pos, char := range sr {
-
 		if unicode.IsDigit(char) && pos == 0 {
 			return "", ErrInvalidString
 		}
 
-		if unicode.IsLetter(char) && len(last_char.String()) == 0 {
-			last_char.WriteString(string(char))
-			full_string.WriteString(string(char))
-			last_char_is_digit = false
+		if unicode.IsLetter(char) && len(lastChar.String()) == 0 {
+			lastChar.WriteString(string(char))
+			fullString.WriteString(string(char))
+			lastCharIsDigit = false
 			continue
 		}
 
-		if unicode.IsLetter(char) && len(last_char.String()) > 0 {
-			last_char.Reset()
-			last_char.WriteString(string(char))
-			full_string.WriteString(string(char))
-			last_char_is_digit = false
+		if unicode.IsLetter(char) && len(lastChar.String()) > 0 {
+			lastChar.Reset()
+			lastChar.WriteString(string(char))
+			fullString.WriteString(string(char))
+			lastCharIsDigit = false
 			continue
 		}
 
-		if unicode.IsDigit(char) && last_char_is_digit == true {
+		if unicode.IsDigit(char) && lastCharIsDigit {
 			return "", ErrInvalidString
 		}
 
-		if unicode.IsDigit(char) && len(last_char.String()) > 0 {
+		if unicode.IsDigit(char) && len(lastChar.String()) > 0 {
 			y, _ := strconv.Atoi(string(char))
 
 			if y == 0 {
-				str := full_string.String()
+				str := fullString.String()
 				str = str[:len(str)-1]
-				full_string.Reset()
-				full_string.WriteString(str)
+				fullString.Reset()
+				fullString.WriteString(str)
 			}
 
 			if y > 0 {
 				for i := 1; i < y; i++ {
-					full_string.WriteString(last_char.String())
+					fullString.WriteString(lastChar.String())
 				}
 			}
-			last_char.Reset()
-			last_char_is_digit = true
+			lastChar.Reset()
+			lastCharIsDigit = true
 			continue
 		}
-
 	}
 
-	return full_string.String(), nil
+	return fullString.String(), nil
 }
