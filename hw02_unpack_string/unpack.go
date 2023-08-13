a624f4c95954fd62f7c3e7b6aaa5537a6d1e8ea6
Кальнов Иван Юрьевич <IYKalnov@sberbank.ru>
2023-08-13 03:33:59 +0300

    done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,8 +1,7 @@
-package main
+package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 	"unicode"
@@ -14,16 +13,18 @@ func Unpack(sr string) (string, error) {
 
 	var last_char strings.Builder
 	var full_string strings.Builder
+	var last_char_is_digit bool
 
 	for pos, char := range sr {
 
-		if pos == 0 && unicode.IsDigit(char) {
+		if unicode.IsDigit(char) && pos == 0 {
 			return "", ErrInvalidString
 		}
 
 		if unicode.IsLetter(char) && len(last_char.String()) == 0 {
 			last_char.WriteString(string(char))
 			full_string.WriteString(string(char))
+			last_char_is_digit = false
 			continue
 		}
 
@@ -31,28 +32,35 @@ func Unpack(sr string) (string, error) {
 			last_char.Reset()
 			last_char.WriteString(string(char))
 			full_string.WriteString(string(char))
+			last_char_is_digit = false
 			continue
 		}
 
-		if unicode.IsDigit(char) {
+		if unicode.IsDigit(char) && last_char_is_digit == true {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(char) && len(last_char.String()) > 0 {
 			y, _ := strconv.Atoi(string(char))
 
-			for i := 1; i < y; i++ {
-				full_string.WriteString(last_char.String())
+			if y == 0 {
+				str := full_string.String()
+				str = str[:len(str)-1]
+				full_string.Reset()
+				full_string.WriteString(str)
+			}
+
+			if y > 0 {
+				for i := 1; i < y; i++ {
+					full_string.WriteString(last_char.String())
+				}
 			}
 			last_char.Reset()
+			last_char_is_digit = true
 			continue
 		}
 
 	}
 
 	return full_string.String(), nil
-}
-
-func main() {
-	a, err := Unpack("ab3a3c6")
-	fmt.Println(a)
-	if err != nil {
-		fmt.Println(err)
-	}
 }
